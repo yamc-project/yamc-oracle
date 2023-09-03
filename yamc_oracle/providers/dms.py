035abfec1f2a556ccf78ef7fc89cbf25c1f47ac8
Tomas Vitvar <tomas@vitvar.com>
2023-09-03 23:28:01 +0200

    thread-safe fix

--- yamc_oracle/providers/dms.py
+++ yamc_oracle/providers/dms.py
@@ -4,12 +4,13 @@
 import time
 
 import dms_collector
+import threading
 from dms_collector import DmsCollector
 from yamc.providers import PerformanceProvider, perf_checker, OperationalError
 
 from dms_collector import TableNotExistError, DataParserError
 
-from yamc.utils import Map, perf_counter
+from yamc.utils import Map
 
 
 class DmsProvider(PerformanceProvider):
@@ -20,59 +21,76 @@ class DmsProvider(PerformanceProvider):
 
     def __init__(self, config, component_id):
         super().__init__(config, component_id)
-        self.connect_time = 0
-        self.dms = None
-        self.force_reconnect = False
+        self.dms = Map()
 
         # configuration
         self.admin_url = self.config.value_str("admin_url", required=True)
         self.username = self.config.value_str("username", required=True)
         self.password = self.config.value_str("password", required=True)
         self.reconnect_after = self.config.value_int("reconnect_after", default=3600)
-
-        # set timeouts for the dms collector
         self.read_timeout = self.config.value_int("timeout_read", default=dms_collector.dms.TIMEOUT_READ)
         self.connect_timeout = self.config.value_int("timeout_connect", default=dms_collector.dms.TIMEOUT_CONNECT)
+        self.log.info(
+            f"dms_collector will use the following connection details: url={self.admin_url}, username={self.username}, "
+            + f"password=******, reconnect_after={self.reconnect_after}, read_timeout={self.read_timeout}, "
+            + f"connect_timeout={self.connect_timeout}"
+        )
 
     @property
     def source(self):
+        """
+        Returns the source of the data, which is the admin URL.
+        """
         return self.admin_url
 
-    def init_dms(self):
-        if (
-            self.force_reconnect
-            or self.dms is None
-            or time.time() - self.connect_time > self.reconnect_after
-            or self.force_reconnect
-        ):
-            if self.dms is not None:
-                self.log.info("Reconnecting to DMS Spy after %d seconds." % self.reconnect_after)
-            if self.force_reconnect:
-                self.log.info("Reconnecting to DMS Spy due to an error.")
-                self.force_reconnect = False
-            self.dms = DmsCollector(
+    def get_dms(self, table):
+        """
+        Returns a DMS object for the given table. If the object does not exist or it is too old, a new object is
+        created. The object is stored in the dms dictionary.
+        """
+        dms_id = str(threading.get_native_id()) + "/" + table
+        dms = self.dms.get(dms_id)
+        if dms is None or dms.force_reconnect or time.time() - dms.connect_time > self.reconnect_after:
+            if dms is not None:
+                if dms.force_reconnect:
+                    self.log.info("Reconnecting to DMS due to an error (id=%s)." % dms_id)
+                else:
+                    self.log.info("Reconnecting to DMS after %d seconds (id=%s)." % (self.reconnect_after, dms_id))
+            dms_collector = DmsCollector(
                 self.admin_url,
                 username=self.username,
                 password=self.password,
                 read_timeout=self.read_timeout,
                 connect_timeout=self.connect_timeout,
             )
-            self.log.info(
-                "DMS provider initialized: url=%s, username=%s, password=(secret), read-timeout=%d, connect-timeout=%d"
-                % (self.admin_url, self.username, self.read_timeout, self.connect_timeout)
+            self.log.info(f"dms_collector created: id={dms_id}")
+            dms = Map(
+                dms_id=dms_id, dms_collector=dms_collector, table=table, connect_time=time.time(), force_reconnect=False
             )
-            self.connect_time = time.time()
+            self.dms[dms_id] = dms
+        return dms
 
     @perf_checker(id_arg="table")
     def table(self, table, include=[], exclude=[], filter=None):
+        """
+        Returns a list of records from the given table. The table is a DMS table name, e.g., "JVMRuntime".
+        The include and exclude parameters are lists of column names to include or exclude from the result.
+        The filter parameter is a dictionary of column names and values to filter the result. The function uses
+        a cache to store the DMS object for each table name and the calling thread.
+        """
         try:
-            self.init_dms()
-            d = self.dms.collect(table, include=include, exclude=exclude, filter=filter)
-            data = d["data"]
-            self.log.info(f"The DMS retrieved {len(data)} records in {d['query_time']:0.4f} seconds from '{table}'.")
-            return data
-        except (DataParserError, TableNotExistError) as e:
-            self.force_reconnect = True
-            raise OperationalError(f"Invalid data retrieved from DMS, will reconnect to DMS on the next run. {e}", e)
+            dms = self.get_dms(table)
+            try:
+                d = dms.dms_collector.collect(table, include=include, exclude=exclude, filter=filter)
+                data = d["data"]
+                self.log.info(
+                    f"The DMS retrieved {len(data)} records in {d['query_time']:0.4f} seconds from '{table}'."
+                )
+                return data
+            except (DataParserError, TableNotExistError) as e:
+                dms.force_reconnect = True
+                raise OperationalError(
+                    f"Invalid data retrieved from DMS, will reconnect to DMS on the next run. {e}", e
+                )
         except Exception as e:
             raise OperationalError(f"Error while retrieving data from DMS: {e}", e)
